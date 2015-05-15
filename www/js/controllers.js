4c9b0c6c32b1332d877840e7aebb00c2d445fbe6
Roman Breslav <rbreslav@Roman-Breslavs-MacBook-Pro.local>
2015-05-15 13:28:43 -0400

    nice work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,70 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('MovesCtrl', function($scope) {})
+.controller('MovesCtrl', function($scope) {
+
+  angular.extend($scope, { 
+
+	moves: {
+
+	  rows: [1,2,3],
+
+	  cols: [1,2,3],
+
+	  currentSquare: "square_1_1",
+
+	  onDropComplete: function(toSquare, event) {
+
+	  	if (this.canDrop(toSquare)) {
+
+			this.currentSquare = toSquare;	  		
+	  	}
+	  },
+
+	  canDrop: function(square) {
+
+		var from = this.getCoords(this.currentSquare);
+
+		var moves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]];
+
+		for (i in moves) {
+
+		  var x = from.x + moves[i][0];
+		  var y = from.y + moves[i][1];
+
+		  if (x > 0 && x < 9 && y > 0 && y < 9 && square === 'square_' + x + '_' + y) {
+
+			return true;
+		  }
+		}
+
+		return false;
+	  },
+
+	  isVisible: function(square) {
+
+		if (square === this.currentSquare) {
+
+		  return true;
+		}
+
+		return false;
+	  },
+
+	  getCoords: function(square) {
+
+		var parts = square.split('_');
+
+		return {
+		  x: parseInt(parts[1]),
+		  y: parseInt(parts[2])
+		}
+	  }
+	}
+
+  });
+
+})
 
 /*
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -23,6 +86,6 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+	enableFriends: true
   };
 });
